d51d198a25cc3ba05283ee6ba7cebadca81a343c
Stone Filipczak <stone@smartlogic.io>
2022-12-23 13:47:28 -0500

    comment out repo stuff from release.ex

--- lib/drums/release.ex
+++ lib/drums/release.ex
@@ -8,9 +8,9 @@ defmodule Drums.Release do
   def migrate do
     load_app()
 
-    for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    end
+    # for repo <- repos() do
+    #   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    # end
   end
 
   def rollback(repo, version) do
@@ -18,9 +18,9 @@ defmodule Drums.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  defp repos do
-    Application.fetch_env!(@app, :ecto_repos)
-  end
+  # defp repos do
+  #   Application.fetch_env!(@app, :ecto_repos)
+  # end
 
   defp load_app do
     Application.load(@app)
